07230f51f7f6e70e4098c2ed095629d4c4332bf6
shakyayush <shakyayush2000@gmail.com>
2023-07-12 01:53:23 -0400

    Docker implementation

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,15 +1,19 @@
 '''Unit Tests for translation'''
 
 import unittest
-from machinetranslation.translator import english_to_french, french_to_english
+from translator import english_to_french, french_to_english
 
 class testEnglishToFrench(unittest.TestCase):
     '''Test English to French translation'''
     def test1(self):
-        '''Test for boy and Hello'''
-        self.assertEqual(english_to_french('boy'), 'garçon')
+        '''Equal Test for boy and Hello'''
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertEqual(english_to_french('boy'), 'garçon')
 
+    def test2(self):
+        '''Negative tests'''
+        self.assertNotEqual(english_to_french('Hello'), 'Hola')
+        self.assertNotEqual(english_to_french('boy'), 'nina')
     
 class testFrenchToEnglish(unittest.TestCase):
     '''Test French to English translation'''
@@ -17,5 +21,10 @@ class testFrenchToEnglish(unittest.TestCase):
         '''Test for garçon and Bonjour'''
         self.assertEqual(french_to_english('garçon'), 'boy')
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+    
+    def test2(self):
+        '''Negative tests'''
+        self.assertNotEqual(french_to_english('garçon'), 'girl')
+        self.assertNotEqual(french_to_english('Bonjour'), 'human')
 
 unittest.main()
